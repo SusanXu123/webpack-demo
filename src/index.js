b18cc0c953e83bcabe697c21ca5c167078583155
xiaojuan <juan.xu@yintech.cn>
2019-02-12 17:15:13 +0800

    feat: lazy loading

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
-//  import _ from 'lodash'
+ import _ from 'lodash'
  import { cube } from './math.js'
 
  if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
  }
-async function getComponent () {
-    const _ = await import( /* webpackChunkName: "lodash" */ 'lodash')
-    var element = document.createElement('pre');
-    element.innerHTML = _.join([
-        'Hello webpack!',
-        '5 cubed is equal to ' + cube(5)
-    ], '\n\n')
-    return element
-}
-getComponent().then(component => {
-    document.body.appendChild(component)
-})
-//  function component() {
-//   var element = document.createElement('pre');
-//   element.innerHTML = _.join([
-//       'Hello webpack!',
-//       '5 cubed is equal to ' + cube(5)
-//   ], '\n\n')
+ function component() {
+  var element = document.createElement('pre');
+  var button = document.createElement('button');
+  var br = document.createElement('br');
+  button.innerHTML = 'Click me and look at the console!';
+  element.innerHTML = _.join([
+      'Hello webpack!',
+      '5 cubed is equal to ' + cube(5)
+  ], '\n\n')
+  element.appendChild(br);
+  element.appendChild(button);
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  // 注意当调用 ES6 模块的 import() 方法（引入模块）时，必须指向模块的 .default 值，因为它才是 promise 被处理后返回的实际的 module 对象。
+  button.onclick = e => import( /* webpackChunkName: "print" */ './print').then(module => {
+    var print = module.default;
+    print();
+  })
+  return element;
+ }
 
-//   return element;
-//  }
-
-//  document.body.appendChild(component());
+ document.body.appendChild(component());
--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-// const webpack = require('webpack')
+const webpack = require('webpack')
 
 module.exports = {
   entry: {
@@ -16,8 +16,8 @@ module.exports = {
     // new webpack.optimize.CommonsChunkPlugin({
     //   name: 'common' // 指定公共 bundle 的名称
     // }),
-    // new webpack.NamedModulesPlugin(),
-    // new webpack.HotModuleReplacementPlugin()
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ],
   output: {
     filename: '[name].bundle.js',
